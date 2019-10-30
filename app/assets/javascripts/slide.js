484a45764bc3518049259a9cdba8eaaf3c1b033e
tomoman61 <inotomo5@gmail.com>
2019-10-30 20:45:29 +0900

    slide完成

--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -1,20 +1,18 @@
 jQuery(document).ready(function(){
   jQuery('.slider-list').slick({
     autoplaySpeed: 3000,
+    arrows: false,
     dots:true,
     autoplay:true,
     infinite: true,
     draggable: true,
-    accessibility: true,
     pauseOnHover: true,
-    zIndex: 0
+    zIndex: 0,
   });
-  // var $slider_container = $('.slider-container'),
-  //     $slider = $('.slider');
-
-// $slider.slick({
-//   appendArrows: $slider_container,
-//   prevArrow: '.slider-arrow.slider-prev fa-icon angle-left',
-//   nextArrow: '.slider-arrow.slider-next i-con angle-right',
-// });
+  $('.slider-prev').on('click',function(){
+    $('.slider-list').slick('slickNext');
+  });
+  $('.slider-next').on('click',function(){
+    $('.slider-list').slick('slickPrev');
+  });
 });
