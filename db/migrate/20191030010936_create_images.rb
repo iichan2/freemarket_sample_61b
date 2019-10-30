1cd4e6f4c96479b1521592205c7a632763c16239
Fumihiko Inoue <my.world.likes.cloud@gmail.com>
2019-10-30 14:42:33 +0900

    Update 20191030010936_create_images.rb

--- db/migrate/20191030010936_create_images.rb
+++ db/migrate/20191030010936_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.text "image_url", null: false
-      t.integer "item_id", , null: false
+      t.integer "item_id", null: false
       t.timestamps
     end
   end
