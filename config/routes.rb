0f743c7ebdab8ba23e661d1e7815fa085039d941
tomoman61 <inotomo5@gmail.com>
2019-11-03 00:11:00 +0900

    商品購入確認ページ一応完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # devise_for :users
   resources :users
-  root 'tests#index'
+  root 'tests#index' 
+  resources :tests do
+    collection do
+      get 'transaction'
+    end
   end
+end
