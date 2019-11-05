debf92baf731c45c8ba30f766f452f7d99b5d901
tsubasa <tonochi.tsu@gmail.com>
2019-11-05 16:19:03 +0900

    ログイン

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,103 @@
+class SignupController < ApplicationController
+  
+  
+
+def create
+  @user = User.new(
+    nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+    email: session[:email],
+    password: session[:password],
+    password_confirmation: session[:password_confirmation],
+    last_name: session[:last_name], 
+    first_name: session[:first_name], 
+    kana_last_name: session[:kana_last_name], 
+    kana_first_name: session[:kana_first_name], 
+    birth_year: session[:birth_year],
+    birth_month: session[:birth_month],
+    birth_day: session[:birth_day],
+    postal_code: session[:postal_code],
+    ken: session[:ken],
+    map: session[:map],
+    banchi: session[:banchi],
+    tel_number: session[:tel_number],
+
+    
+  )
+  
+  if @user.save
+  # ログインするための情報を保管
+    session[:id] = @user.id
+    redirect_to done_signup_index_path
+  end
+end
+def mail
+  # ログイン新規ページ
+end
+  def new
+    @user = User.new
+  end
+
+  def tel
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name] = user_params[:last_name]
+    session[:kana_first_name] = user_params[:kana_first_name]
+    session[:kana_last_name]= user_params[:kana_last_name]
+    session[:birth_year] = user_params[:birth_year]
+    session[:birth_month] = user_params[:birth_month]
+    session[:birth_day] = user_params[:birth_day]
+    @user = User.new
+  end
+
+# sessionに渡された値をインスタンスに渡す
+  def juusyo
+    session[:tel_number] = user_params[:tel_number]
+    @user = User.new
+  end
+  def card
+    session[:first_name] = user_params[:first_name]
+    session[:last_name] = user_params[:last_name]
+    session[:kana_first_name] = user_params[:kana_first_name]
+    session[:kana_last_name]= user_params[:kana_last_name]
+    session[:postal_code]= user_params[:postal_code]
+    session[:ken]= user_params[:ken]
+    session[:map]= user_params[:map]
+    session[:banchi]= user_params[:banchi]
+    session[:building]= user_params[:building]
+    session[:tel_number]= user_params[:tel_number]
+    @user = User.new
+  end
+  def ok 
+  
+    sign_in User.find(session[:id]) unless user_signed_in?
+  end
+  
+  
+ 
+  private
+  
+  def user_params
+    params.require(:user).permit(
+      :nickname,
+      :last_name, 
+      :first_name, 
+      :last_name_kana, 
+      :first_name_kana,
+      :email,
+      :tel_number,
+      :birth_month,
+      :birth_year,
+      :birth_day,
+      :postal_code,
+      :ken,
+      :map,
+      :banchi,
+      :building,
+      )
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   resources :items, only: [:index,:new, :create]
 
-
+  resources :signup do
+    collection do
+      get 'new'
+      get 'tel'
+      get 'juusyo'
+      get 'card'
+      get 'ok' # ここで、入力の全てが終了する
+    end
+  end
   resources :users do
     collection do
-      get "tel"
-      get "juusyo"
-      get "card"
-      get "ok"
+      
       get "logout"
       get "payment"
     end
