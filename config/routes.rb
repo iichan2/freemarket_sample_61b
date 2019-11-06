8b878c8266a2e8c4eeb351b2d12c6afc7bf65298
hiroki000024 <king0000240000@gmail.com>
2019-11-06 15:25:21 +0900

    商品詳細ページ　マークアップ完了

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       get "identification"
     end
   end
-    root 'tests#index'
+    root 'items#show'
     resources :tests do
       collection do
         get 'transaction'
