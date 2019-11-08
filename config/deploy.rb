103d11048cd7402fcad0808327bf717cfbb0bf5e
iichan2 <hanikanda@yahoo.co.jp>
2019-11-08 18:20:05 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,9 +30,9 @@ after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :db_seed do
     on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, db:seed
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :bundle, :exec, :rake, ‘db:seed’
         end
       end
     end
