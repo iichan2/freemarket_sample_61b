b5f7ea1b0e7661e9ce83c579b6bd52fda533a44d
Fumihiko Inoue <my.world.likes.cloud@gmail.com>
2019-11-09 16:09:19 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-set :default_env, {
+set :default_env, -> {
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
