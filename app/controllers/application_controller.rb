c8e34ce67d97020faf8fa1c33f0f6ac3ec9876dc
Fumihiko Inoue <my.world.likes.cloud@gmail.com>
2019-11-09 16:31:02 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
-        username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+        username == ENV.fetch('BASIC_AUTH_USER', 'nu') && password == ENV.fetch('BASIC_AUTH_PASSWORD', 'ha')
       end
   end 
 
