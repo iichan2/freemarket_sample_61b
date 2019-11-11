d8f8477032bfaf691ffe3c4a2e4589c87fceae89
tsubasa <tonochi.tsu@gmail.com>
2019-11-11 12:11:18 +0900

    三段階ログインの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   # belongs_to :bank, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, through: :items
-  # has_many :reviews, dependent: :destroy
-  # has_one :delivery, dependent: :destroy
+  has_many :sns_credentials, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   # validates :postal_code, presence: true, format: { with: /A\d{3}-\d{4}\z/}
-  # validates :ken, presence: true
-  # validates :map, presence: true
-  # validates :nickname, presence: true, length: { minimum: 1, maximum: 20}
-  # validates :email, presence: true
+  validates :ken, presence: true
+  validates :map, presence: true
+  validates :nickname, presence: true, length: { minimum: 1, maximum: 20}
+  validates :email, presence: true
   # validates :first_name, :last_name,  presence: true
   # validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   # validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
@@ -24,11 +24,50 @@ class User < ApplicationRecord
   # # has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   # has_many :saling_items, -> { where("buyer_id is NULL") }, foreign_key: "saler_id", class_name: "Item"
   # has_many :sold_items, -> { where("buyer_id is not NULL") }, foreign_key: "saler_id", class_name: "Item"
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    snscredential = SnsCredential.where(uid: uid, provider: provider).first
+    # binding.pry
+
+    if snscredential.present? #sns登録のみ完了してるユーザー
+      user = User.where(id: snscredential.user_id).first
+      unless user.present? #ユーザーが存在しないなら
+        user = User.new(
+          # snsの情報
+          # binding.pry => auth.infoとかで確認 
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+      end
+      sns = snscredential
+      #binding.pry
+
+    else #sns登録 未
+      user = User.where(email: auth.info.email).first
+      if user.present? #会員登録 済
+        sns = SnsCredential.new(
+          uid: uid,
+          provider: provider,
+          user_id: user.id
+        )
+      else #会員登録 未
+        user = User.new(
+          nickname: auth.info.name,
+          email: auth.info.email
+        )
+
+        sns = SnsCredential.create(
+          uid: uid,
+          provider: provider
+        )
+        # binding.pry 
+      end
     end
+    # hashでsnsのidを返り値として保持しておく
+    return { user: user , sns_id: sns.id }
   end
 end
 
