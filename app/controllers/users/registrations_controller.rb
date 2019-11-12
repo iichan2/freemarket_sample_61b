fde3b8755534f70ee1ca2aa2a7397da94c413faf
tsubasa <tonochi.tsu@gmail.com>
2019-11-12 21:27:09 +0900

    ビュー修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       params[:user][:password_confirmation] = Devise.friendly_token.first(6)
       super
       sns = SnsCredential.update(user_id:  @user.id)
+      
     else #email登録なら
       super
     end
