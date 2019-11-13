61b63b649cca90dea24dd06090b8a5fac3f447f8
iichan2 <hanikanda@yahoo.co.jp>
2019-11-13 17:36:52 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
                 registrations: 'users/registrations' }
-  resources :categories
+  resources :categories, only: [:index]
   resources :cards
   
   # devise_scope :user do
