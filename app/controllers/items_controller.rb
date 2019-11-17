b055b1946e8e6d2ba1348f1e450a372df24bea29
tsubasa <tonochi.tsu@gmail.com>
2019-11-17 13:32:23 +0900

    変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,10 +10,10 @@ class ItemsController < ApplicationController
     @shioya_items = Item.where(brand_id: 2).limit(10)
     @tonochi_items = Item.where(brand_id: 5).limit(10)
     # トノチ記載↓
+
+
     
-    
-    
-    @item.update(buyer_id: @user.id)
+  
   end
 
   def new
@@ -138,7 +138,7 @@ class ItemsController < ApplicationController
     ken_to_name = ["海外","北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
     @del = "#{ken_to_name[@buyer.delivery.ken]} #{@buyer.delivery.map} #{@buyer.delivery.banchi} #{@buyer.delivery.building}"
   end
-
+  
   private
   def item_params
     params.require(:item).permit(:item_name, :item_info, :category_id, :status, :delivery_fee, :delivery_way, :area, :delivery_day, :price, :exhibition_state,images_attributes: [:image_url]).merge(user_id: current_user.id)
