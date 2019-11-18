0c7f9534cad417a39d2deb246a9ce762e239d97c
tomoman61 <inotomo5@gmail.com>
2019-11-18 13:04:02 +0900

    本人情報編集

--- app/controllers/delivery_controller.rb
+++ app/controllers/delivery_controller.rb
@@ -1,2 +1,23 @@
 class DeliveryController < ApplicationController
+  def du_update
+    @delivery = Delivery.find(params[:id])
+    @delivery.update(
+      postal_code: params[:postal_code],
+      ken: params[:ken],
+      map: params[:map],
+      banchi: params[:banchi],
+      building: params[:building],
+    )
+    if @delivery.update(
+      postal_code: params[:postal_code],
+      ken: params[:ken],
+      map: params[:map],
+      banchi: params[:banchi],
+      building: params[:building],
+    )
+      redirect_to user_path
+    else
+      render :identification
+    end
+  end
 end
