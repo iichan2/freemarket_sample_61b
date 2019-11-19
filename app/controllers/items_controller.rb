ebdc29a24e2d16c9e880000fc8b532b7d995b6b7
Fumihiko Inoue <my.world.likes.cloud@gmail.com>
2019-11-19 16:54:43 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,100 +19,97 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @image = Image.new
-    # @item.images.build
     @category_parents = Category.where(ancestry: nil).map{|i| [i.category, i.id]}
   end
 
   def edit
-
     @item = Item.find(params[:id])
-    @category_parents = Category.where(ancestry: nil).map{|i| [i.category, i.id]}
-    @category_grandchild = Category.find(@item.category_id)
-    @category_child = @category_grandchild.parent
-    @category_parent = @category_child.parent
-    @parents = Category.where(ancestry: nil)
-    @image = @item.images 
-    @category_gc_now = Category.find(@item.category_id)
-    @category_c_now = @category_gc_now.parent
-    @category_p_now = @category_c_now.parent
-    @p_c_children = @category_p_now.children
-    @c_gc_children = @category_c_now.children
-
+    item_user = User.find(@item.user_id)
+    if item_user.id == current_user.id
+      @category_parents = Category.where(ancestry: nil).map{|i| [i.category, i.id]}
+      @category_grandchild = Category.find(@item.category_id)
+      @category_child = @category_grandchild.parent
+      @category_parent = @category_child.parent
+      @parents = Category.where(ancestry: nil)
+      @image = @item.images 
+      @category_gc_now = Category.find(@item.category_id)
+      @category_c_now = @category_gc_now.parent
+      @category_p_now = @category_c_now.parent
+      @p_c_children = @category_p_now.children
+      @c_gc_children = @category_c_now.children
+    else
+      redirect_to root_path
+    end
   end
- 
-  # def create 
-    # item = Item.create(item_params) 
-    # @item = item.exhibition_state = "出品中" 
-      # if @item.save 
-        # redirect_to action: :index 
-      # else 
-        # redirect_to action: :new 
-      # end 
-  # end 
- 
+
   def get_category_children 
     @category_children = Category.find(params[:parent_id]).children 
   end 
- 
+
   def get_category_grandchildren 
     @category_grandchildren = Category.find(params[:child_id]).children 
   end 
   
   def update
     @item = Item.find(params[:id])
-    if params[:item][:images_attributes][:"0"][:image_url].nil?
-      if @item.update!(update_item_params)
-        redirect_to status_sell_user_path(current_user.id)
+    item_user = User.find(@item.user_id)
+    if item_user.id == current_user.id
+      if params[:item][:images_attributes][:"0"][:image_url].nil?
+        if @item.update!(update_item_params)
+          redirect_to status_sell_user_path(current_user.id)
+        else
+          redirect_to controller: "items", action: "edit", id:"#{@item.id}"
+        end
       else
-        redirect_to controller: "items", action: "edit", id:"#{@item.id}"
+        if @item.update!(update_item_params_without_image)
+          uploaded_files = [
+            params[:item][:images_attributes][:"0"],
+            params[:item][:images_attributes][:"1"],
+            params[:item][:images_attributes][:"2"],
+            params[:item][:images_attributes][:"3"],
+            params[:item][:images_attributes][:"5"],
+            params[:item][:images_attributes][:"4"],
+            params[:item][:images_attributes][:"6"],
+            params[:item][:images_attributes][:"7"],
+            params[:item][:images_attributes][:"8"],
+            params[:item][:images_attributes][:"9"]
+          ]
+          num = 1
+          uploaded_files.each do |uf|
+            if uf.nil?
+              num += 1
+            else
+              date = DateTime.now.strftime('%Y%m%d%H%M%S').to_i
+              if uf[:image_url].kind_of?(Array)
+                uff = uf[:image_url][0]
+              else
+                uff = uf[:image_url]
+              end
+              if uff.nil?
+                num += 1
+              else
+                output_path = Rails.root.join('public', "#{@item.id}", "#{date + num}")
+                File.open(output_path, 'w+b') do |fp|
+                  fp.write  uff.read
+                end
+                save_path = "/#{@item.id}/#{date + num}"
+                @image = Image.create(item_id:@item.id,image_url:save_path)
+                num += 1
+              end 
+            end
+          end
+          redirect_to status_sell_user_path(current_user.id)
+        else
+          redirect_to controller: "items", action: "edit", id:"#{@item.id}"
+        end
+        # @category_parents = Category.where(ancestry: nil).map{|i| [i.category, i.id]}
+        # @category_grandchild = Category.find(@item.category_id)
+        # @category_child = @category_grandchild.parent
+        # @category_parent = @category_child.parent
+        # redirect_to edit_item_path(@item)
       end
     else
-      if @item.update!(update_item_params_without_image)
-        uploaded_files = [
-          params[:item][:images_attributes][:"0"],
-          params[:item][:images_attributes][:"1"],
-          params[:item][:images_attributes][:"2"],
-          params[:item][:images_attributes][:"3"],
-          params[:item][:images_attributes][:"5"],
-          params[:item][:images_attributes][:"4"],
-          params[:item][:images_attributes][:"6"],
-          params[:item][:images_attributes][:"7"],
-          params[:item][:images_attributes][:"8"],
-          params[:item][:images_attributes][:"9"]
-        ]
-        num = 1
-        uploaded_files.each do |uf|
-          if uf.nil?
-            num += 1
-          else
-            date = DateTime.now.strftime('%Y%m%d%H%M%S').to_i
-            if uf[:image_url].kind_of?(Array)
-              uff = uf[:image_url][0]
-            else
-              uff = uf[:image_url]
-            end
-            if uff.nil?
-              num += 1
-            else
-              output_path = Rails.root.join('public', "#{@item.id}", "#{date + num}")
-              File.open(output_path, 'w+b') do |fp|
-                fp.write  uff.read
-              end
-              save_path = "/#{@item.id}/#{date + num}"
-              @image = Image.create(item_id:@item.id,image_url:save_path)
-              num += 1
-            end 
-          end
-        end
-        redirect_to status_sell_user_path(current_user.id)
-      else
-        redirect_to controller: "items", action: "edit", id:"#{@item.id}"
-      end
-      # @category_parents = Category.where(ancestry: nil).map{|i| [i.category, i.id]}
-      # @category_grandchild = Category.find(@item.category_id)
-      # @category_child = @category_grandchild.parent
-      # @category_parent = @category_child.parent
-      # redirect_to edit_item_path(@item)
+      redirect_to root_path
     end
   end
   
@@ -250,6 +247,7 @@ class ItemsController < ApplicationController
       save_path = "/#{item_id}/0"
       @image = Image.create(item_id:item_id,image_url:save_path)
     end
+
     checknil = params[:item][:images_attributes]
     if checknil.nil?
     else
@@ -294,9 +292,7 @@ class ItemsController < ApplicationController
   def update_item_params_without_image
     params.require(:item).permit(:item_name, :item_info, :category_id, :status, :delivery_fee, :delivery_way, :area, :delivery_day, :price).merge(user_id: current_user.id)
   end
-  # def image_params
-  #   params.require(:item).permit(:image[:image_url])
-  # end
+
   def comment_params
     params.require(:comment).permit(:text,:item_id).merge(user_id: current_user.id)
   end
