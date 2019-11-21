9840acb4061fa32fb54410658492fd6bafb4567d
tsubasa <tonochi.tsu@gmail.com>
2019-11-21 17:32:05 +0900

    アクティブハッシュに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -121,8 +121,9 @@ class ItemsController < ApplicationController
   def transaction
     if user_signed_in?
       @user = User.find(current_user.id)
-      ken_to_name = ["海外","北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
-      @del = "#{ken_to_name[@user.delivery.ken]} #{@user.delivery.map} #{@user.delivery.banchi} #{@user.delivery.building}"
+      @delivery = Delivery.find(current_user.id)
+      @kenname = Prefecture.find(@user.delivery.ken).name
+      @del = "#{@kenname} #{@user.delivery.map} #{@user.delivery.banchi} #{@user.delivery.building}"
       @item = Item.find(params[:id])
       @image = @item.images.first
       if @item.exhibition_state == "出品中"
