6a4d5831b27aca0d248196e3863ce4f5377157f8
tsubasa <tonochi.tsu@gmail.com>
2019-11-22 17:44:45 +0900

    バリデーション

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,5 +1,7 @@
 class Delivery < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   belongs_to :user
 end
