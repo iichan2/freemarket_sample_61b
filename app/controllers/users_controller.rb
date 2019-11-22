2f7a28f6cf2cefca79952241fe1181f446ff37a5
iichan2 <hanikanda@yahoo.co.jp>
2019-11-22 13:34:59 +0900

    リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,63 +34,50 @@ class UsersController < ApplicationController
   end
 
   def status_sell
-    items = Item.where(user_id: @user.id)
-    able_items = items.where("(exhibition_state = ?) OR (exhibition_state = ?)", "出品中", "停止中")
-    @items_images = []
-    able_items.each do |item|
-      arry = Image.where(item_id: item.id)
-      image = arry.first
-      hash = {item: item,image: image}
-      @items_images << hash
-    end
+    exhibition_state = "出品中"
+    who_sold = "user"
+    @items_images = create_one_item_one_image(exhibition_state,who_sold)
   end
 
   def status_trading
-    items = Item.where(user_id: @user.id)
-    able_items = items.where(exhibition_state: "取引中")
-    @items_images = []
-    able_items.each do |item|
-      arry = Image.where(item_id: item.id)
-      image = arry.first
-      hash = {item: item,image: image}
-      @items_images << hash
-    end
+    exhibition_state = "取引中"
+    who_sold = "user"
+    @items_images = create_one_item_one_image(exhibition_state,who_sold)
   end
 
   def status_sold
-    items = Item.where(user_id: @user.id)
-    able_items = items.where(exhibition_state: "売却済")
-    @items_images = []
-    able_items.each do |item|
-      arry = Image.where(item_id: item.id)
-      image = arry.first
-      hash = {item: item,image: image}
-      @items_images << hash
-    end
+    exhibition_state = "売却済"
+    who_sold = "user"
+    @items_images = create_one_item_one_image(exhibition_state,who_sold)
   end
   
   def status_delivery
-    items = Item.where(buyer_id: @user.id)
-    able_items = items.where(exhibition_state: "取引中")
-    @items_images = []
-    able_items.each do |item|
-      arry = Image.where(item_id: item.id)
-      image = arry.first
-      hash = {item: item,image: image}
-      @items_images << hash
-    end
+    exhibition_state = "取引中"
+    who_sold = "buyer"
+    @items_images = create_one_item_one_image(exhibition_state,who_sold)
   end
 
   def status_bought
-    items = Item.where(buyer_id: @user.id)
-    able_items = items.where(exhibition_state: "売却済")
-    @items_images = []
+    exhibition_state = "売却済"
+    who_sold = "buyer"
+    @items_images = create_one_item_one_image(exhibition_state,who_sold)
+  end
+
+  def create_one_item_one_image(exhibition_state,who)
+    if who == "user"
+      items = Item.where(user_id: current_user.id)
+    else
+      items = Item.where(buyer_id: current_user.id)
+    end
+    able_items = items.where(exhibition_state: exhibition_state)
+    items_images = []
     able_items.each do |item|
       arry = Image.where(item_id: item.id)
       image = arry.first
       hash = {item: item,image: image}
-      @items_images << hash
+      items_images << hash
     end
+    return items_images
   end
 
   def logout
